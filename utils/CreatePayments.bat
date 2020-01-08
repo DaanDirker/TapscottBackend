87d7742d192a3182360b6d970a3ce04e2553682d
Remy <rderuiter@hotmail.nl>
2020-01-08 23:25:34 +0100

    Batch File with curl commands to execute some test payments

---
+++ utils/CreatePayments.bat
@@ -0,0 +1,13 @@
+curl --data "" http://localhost:3000/payment/IBAN1/1000
+curl --data "" http://localhost:3000/payment/IBAN2/800
+curl --data "" http://localhost:3000/payment/IBAN3/900
+curl --data "" http://localhost:3000/payment/IBAN4/1200
+curl --data "" http://localhost:3000/payment/IBAN5/1500
+curl --data "" http://localhost:3000/payment/IBAN3/225
+curl --data "" http://localhost:3000/payment/IBAN1/500
+curl --data "" http://localhost:3000/payment/IBAN2/1224.95
+curl --data "" http://localhost:3000/payment/IBAN5/750
+curl --data "" http://localhost:3000/payment/IBAN4/3500
+curl --data "" http://localhost:3000/payment/IBAN3/1375
+
+pause
